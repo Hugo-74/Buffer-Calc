87737bfb50a6c9c3b2a292c44badfbf47eb1f905
Hugo <154477470+Hugo-74@users.noreply.github.com>
2026-03-11 14:56:01 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,29 @@
-const CACHE_NAME = 'benchmate-v1';
+const CACHE_NAME = 'benchmate-v2';
 const urlsToCache = [
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
-// Installation du Service Worker et mise en cache
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Récupération depuis le cache (Mode hors-ligne)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Retourne la version en cache si elle existe, sinon fait la requête réseau
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
-
 });
-
